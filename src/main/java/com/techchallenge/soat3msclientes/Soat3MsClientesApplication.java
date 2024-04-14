a413dd8cd15b71247d3db8f6881823623cb9e9ea
VinniJr <vinni.jr@gmail.com>
2024-04-14 20:21:23 -0300

    Primeiro commit

---
+++ src/main/java/com/techchallenge/soat3msclientes/Soat3MsClientesApplication.java
@@ -0,0 +1,13 @@
+package com.techchallenge.soat3msclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soat3MsClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Soat3MsClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techchallenge/soat3msclientes/Soat3MsClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techchallenge.soat3msclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Soat3MsClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
