85f2cf3c1442e78331804fed3b5d6dc86f0bc856
Francisco Viana <franciscosantos29@gmail.com>
2024-05-09 18:02:49 -0300

    config sonarcloud

--- src/test/java/com/techchallenge/soat3msclientes/Soat3MsClientesApplicationTests.java
+++ src/test/java/com/techchallenge/soat3msclientes/Soat3MsClientesApplicationTests.java
@@ -3,7 +3,7 @@ package com.techchallenge.soat3msclientes;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Soat3MsClientesApplicationTests {
 
 	@Test
