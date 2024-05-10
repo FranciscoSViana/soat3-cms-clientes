a1a64ee58ba30a0a6128f8e52a0b40e0bc465583
Francisco Viana <franciscosantos29@gmail.com>
2024-05-10 13:53:53 -0300

    refatorar para os testes e passar no sonarcloud

--- src/test/java/com/techchallenge/soat3msclientes/Soat3MsClientesApplicationTests.java
+++ src/test/java/com/techchallenge/soat3msclientes/Soat3MsClientesApplicationTests.java
@@ -1,9 +1,7 @@
 package com.techchallenge.soat3msclientes;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class Soat3MsClientesApplicationTests {
