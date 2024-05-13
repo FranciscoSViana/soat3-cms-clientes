439b496bcd6aa3662c7f029ee127615545c4bd73
Francisco Viana <franciscosantos29@gmail.com>
2024-05-13 09:28:23 -0300

    criação manifestos yaml

--- src/main/java/com/techchallenge/soat3msclientes/Soat3MsClientesApplication.java
+++ src/main/java/com/techchallenge/soat3msclientes/Soat3MsClientesApplication.java
@@ -2,9 +2,12 @@ package com.techchallenge.soat3msclientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-
+@EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Soat3MsClientesApplication {
 
 	public static final String PACKAGE = "com.techchallenge.soat3msclientes";
